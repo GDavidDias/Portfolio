82b38bf83a0c06c37c6b8531b2c23029f2652bd7
GDavidDias <guillermo.david.dias@gmail.com>
2023-08-21 10:43:40 -0300

    path

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <>
         {<Navbar/>}
         <Routes>
-            <Route exact path='/Home' element={<Home/>} />
+            <Route exact path='/' element={<Home/>} />
         </Routes>
     </>
   )
