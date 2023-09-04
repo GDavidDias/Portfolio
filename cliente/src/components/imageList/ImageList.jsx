90a34925b46d11db37a04f0bd8abae5ad99f46ec
GDavidDias <guillermo.david.dias@gmail.com>
2023-09-04 01:38:48 -0300

    new proyect ecommerce

--- cliente/src/components/imageList/ImageList.jsx
+++ cliente/src/components/imageList/ImageList.jsx
@@ -1,9 +1,10 @@
 import image1 from '../../../assets/images/image1.jpg';
 import image2 from '../../../assets/images/image2.jpg';
 import image3 from '../../../assets/images/image3.jpg';
+import image4 from '../../../assets/images/image4.jpg';
 
 const ImageList = {
-    image1,image2,image3
+    image1,image2,image3,image4
 };
 
 export default ImageList;
