2fee087ae9d6480f9ecab60540d1e2fe3e3c1b43
GDavidDias <guillermo.david.dias@gmail.com>
2023-11-09 14:44:22 -0300

    img

--- cliente/src/components/imageList/ImageList.jsx
+++ cliente/src/components/imageList/ImageList.jsx
@@ -4,6 +4,7 @@ import image3 from '../../../assets/images/image3.jpg';
 import image4 from '../../../assets/images/image4.jpg';
 import image5 from '../../../assets/images/image5.jpg';
 
+
 const ImageList = {
     image1,image2,image3,image4,image5,
 };
